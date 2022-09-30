c12b981fdfaa2889492d6fd13ae14530be88d8c3
Luis Marin <lemaguilera@gmail.com>
2022-09-30 15:23:55 -0500

    replace fastify with express in local dev server

--- src/cmdDev.ts
+++ src/cmdDev.ts
@@ -1,51 +1,68 @@
-import fastify from "fastify";
-import fastifyStatic from "fastify-static";
+import express from "express";
+import { Server } from "http";
 import watch from "node-watch";
-import { resolve } from "path";
 import { EventEmitter } from "stream";
 import { build } from "./build";
 import { fileExists } from "./fileExist";
 import { getOptions } from "./getOptions";
 
-
-export const dev = async (sourceDir: string, outputDir: string, port = 5000, addr = "127.0.0.1") => {
-    const opts = getOptions({ outputDir, sourceDir });
-    sourceDir = opts.sourceDir;
-    outputDir = opts.outputDir;
-    if (! await fileExists(sourceDir)) throw `source directory doesn't exist`;
-    await build({ sourceDir, outputDir, hotRestart: true });
-    let building = false;
-    let rebuild = false;
-    const events = new EventEmitter();
-    const watcher = watch(sourceDir, { recursive: true }, async (_event, _) => {
-        if (building) {
-            rebuild = true;
-            return;
-        }
-        building = true;
-        await build({ sourceDir, outputDir, hotRestart: true });
-        if (rebuild) await build({ sourceDir, outputDir, hotRestart: true });
-        events.emit("built");
-        building = false;
-    });
-    const server = fastify();
-    let firstRequest = true;
-    server.get("/compdate.txt", (_request, reply) => {
-        if (firstRequest) {
-            firstRequest = false;
-            reply.send({ reload: true });
-            return;
-        }
-        const lastListener = () => reply.send({ reload: true });
-        events.once("built", lastListener);
-    });
-    server.register(fastifyStatic, { root: resolve(outputDir), extensions: ["html"] });
-    try {
-        server.listen(port, addr, () => console["log"](`linstening on ${addr}:${port}`));
-    } catch (_) {
-        console["log"](_);
+export const dev = async (
+  sourceDir: string,
+  outputDir: string,
+  port = 5000,
+  addr = "127.0.0.1"
+) => {
+  const opts = getOptions({ outputDir, sourceDir });
+  sourceDir = opts.sourceDir;
+  outputDir = opts.outputDir;
+  if (!(await fileExists(sourceDir))) throw `source directory doesn't exist`;
+  await build({ sourceDir, outputDir, hotRestart: true });
+  let building = false;
+  let rebuild = false;
+  const events = new EventEmitter();
+  const watcher = watch(sourceDir, { recursive: true }, async (_event, _) => {
+    if (building) {
+      rebuild = true;
+      return;
     }
-    return { closeWatcher: () => watcher.close(), closeServer: () => server.close() };
+    building = true;
+    await build({ sourceDir, outputDir, hotRestart: true });
+    if (rebuild) await build({ sourceDir, outputDir, hotRestart: true });
+    events.emit("built");
+    building = false;
+  });
+  const app = express();
+  app.use(express.static(outputDir));
+
+  let firstRequest = true;
+  app.get("/compdate.txt", (_request, reply) => {
+    if (firstRequest) {
+      firstRequest = false;
+      reply.send({ reload: true });
+      return;
+    }
+    const lastListener = () => reply.send({ reload: true });
+    events.once("built", lastListener);
+  });
+  let server: Server;
+  try {
+    server = app.listen(port, addr, () =>
+      console["log"](`linstening on ${addr}:${port}`)
+    );
+  } catch (_) {
+    console["log"]("error starting local dev server", _);
+  }
+  return {
+    closeWatcher: () => watcher.close(),
+    closeServer: () => server.close(),
+  };
 };
 
-export const cmdDev = async (sourceDir: string, outputDir: string, port = 5000, addr = "127.0.0.1") => { await dev(sourceDir, outputDir, port, addr); }
+export const cmdDev = async (
+  sourceDir: string,
+  outputDir: string,
+  port = 5000,
+  addr = "127.0.0.1"
+) => {
+  await dev(sourceDir, outputDir, port, addr);
+};
