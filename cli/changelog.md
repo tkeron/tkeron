bf3f80362677586aaa02f3821d13f256568c8258
lemaguilera <lemaguilera@gmail.com>
2023-10-05 17:38:35 -0500

    fix fetch error

--- cli/changelog.md
+++ cli/changelog.md
@@ -1,3 +1,11 @@
+# v0.0.6
+
+- fixed fetch error.
+
+# v0.0.5
+
+- move to cli directory, action runs on directory changes.
+
 # v0.0.4
 
 - update bundleTs function to allow delete outfile.
---
+++ cli/src/jsDom/fetch.ts
@@ -0,0 +1,13 @@
+import { DOMWindow } from "jsdom";
+
+export const ext_fetch = (window: DOMWindow) => {
+  //@ts-ignore
+  window.fetch = async () => {
+    return {
+      json: async () => ({}),
+      text: async () => "",
+    } as Response;
+  };
+};
+
+
