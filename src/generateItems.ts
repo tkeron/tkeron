b89c88d277554c366b5d024cba0bd54dfac1f9b9
Luis Marin <lemaguilera@gmail.com>
2024-12-02 14:35:41 -0500

    page and componets items updated, closes #67

--- src/generateItems.ts
+++ src/generateItems.ts
@@ -1,11 +1,11 @@
 export const componentItem = (name: string) => ({
-  ts: `import { tkeron, Component } from "@tkeron";\nimport css from "./${name}.css";\n\nexport interface ${name} extends Component {\n    \n}\n\nexport const ${name} = () => {\n    const com = tkeron("${name}") as ${name};\n    \n    return com;\n};\ntkeron.css("${name}",css);\n`,
+  ts: `import { addCss, tk, TkeronElement } from "@tkeron";\nimport css from "./${name}.css";\n\n\nexport const ${name} = (): TkeronElement => {\n    const com = tk.addClass("${name}");\n\n    return com;\n};\n\naddCss(css);\n`,
   css: `.${name} {\n    \n}\n`,
 });
 export const pageItem = (name: string) => ({
-  html: `<!DOCTYPE html>\n<html lang="en">\n    <head>\n    <meta charset="UTF-8" />\n    <meta http-equiv="X-UA-Compatible" content="IE=edge" />\n    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n    <title>${name}</title>\n    </head>\n    <body></body>\n</html>\n`,
-  ts: `import { tkeron } from "@tkeron";\n\ntkeron({type:"h1",value:"${name} page front works"}).appendIn("body");`,
-  back: `import { tkeron } from "@tkeron";\n\ntkeron({type:"h1",value:"${name} page back works"}).appendIn("body");`,
+  html: `<!DOCTYPE html>\n<html lang="en">\n\n<head>\n    <meta charset="UTF-8" />\n    <meta http-equiv="X-UA-Compatible" content="IE=edge" />\n    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n    <title>${name}</title>\n</head>\n\n<body></body>\n\n</html>`,
+  ts: `import { tk } from "@tkeron";\n\n\ntk("h1").setText("h1 rendered in front (browser)").appendIn.body;\n`,
+  back: `import { addCss, tk } from "@tkeron";\nimport css from "./${name}.page.css";\n\n\naddCss(css);\n\ntk("h1").setText("h1 rendered in back (pre rendered)").appendIn.body;\n`,
   css: `html, body {\n    margin: 0;\n    padding: 0;\n}\n`,
 });
 
