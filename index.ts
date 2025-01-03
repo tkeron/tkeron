03349df77eca46a440bb3b1419486c11a9402662
Luis Marin <lemaguilera@gmail.com>
2025-01-02 21:50:08 -0500

    building index

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+import { getCommands } from "@tkeron/commands";
+import p from "./package.json" assert { type: "json" };
+import { ASCII } from "./src/ASCII";
+
+
+
+const cmd = getCommands();
+
+cmd.addHeaderText(ASCII)
+cmd.programName = p.name;
+cmd.version = p.version;
+
+
+cmd.addCommand("build")
+    .addDescription("Build the project in output directory")
+    .addAlias("b")
+    .setCallback(() => {
+        console.log("Building the project...");
+    });
+
+
+cmd.start();
